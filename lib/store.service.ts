d0b3c50c6ca001168bce657e3db889caf2aeb0c1
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-12-07 16:10:49 -0800

    Changed Key History

--- lib/store.service.ts
+++ lib/store.service.ts
@@ -243,9 +243,12 @@ export class StoreService {
         const changedKeyPaths = Object.keys(changedKeyPathsHistory)
 
         // If there were attempts to change locked keys, console log an array of the would-be affected locked keys and return a deep clone of state.
-        const changedLockedKeys = changedKeyPaths.filter(e => this.lockedKeys.indexOf(e) > -1)
+        const changedLockedKeys = {}
+        for (let key in this.lockedKeys) {
+          if (key in changedKeyPathsHistory) changedLockedKeys[key] = changedKeyPathsHistory[key]
+        }
 
-        if (changedLockedKeys.length) {
+        if (Object.keys(changedLockedKeys).length) {
             if (this.mode === 'dev' || this.mode === 'devlite') {
                 console.log("%cState change operation rejected: Cannot change locked keys:", this.styles['cannotMutateState'], ...changedLockedKeys)
                 console.groupEnd()
